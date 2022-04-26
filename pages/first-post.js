f4e331a308bb9d218f7c2c1d7f5a92882e74902d
trishapakhi <vijaya.mistri@gmail.com>
2022-04-26 13:08:54 +0530

    change post ui

---
+++ pages/first-post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Post1st from '../component/post/PostSection/1stpost'
+
+const FirstPost = ({ props }) => {
+  return (
+    // <div></div>
+    <Post1st props={props} />
+  )
+}
+
+export default FirstPost
